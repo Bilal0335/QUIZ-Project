a1910b7313ba7903dd6d79b76496b9a16402d68d
Bilal0335 <bilalhussain42201.com>
2024-07-31 00:53:01 +0500

    update

---
+++ dist/index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const apiLink = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple";
+async function main() {
+    console.log(chalk.yellowBright.bold("=".repeat(60)));
+    console.log(chalk.yellowBright.bold("        **BilalCode - Quiz Project**          "));
+    console.log(chalk.yellowBright.bold("=".repeat(60)));
+}
+await main();
+let fetchData = async (data) => {
+    let fetchQuiz = await fetch(data);
+    let res = await fetchQuiz.json();
+    return res.results;
+};
+let data = await fetchData(apiLink);
+function totlecaseName(name) {
+    return name.replace(/\b\w/g, (char) => char.toUpperCase());
+}
+let startQuiz = async () => {
+    let scoreNum = 0;
+    //for username
+    let name = await inquirer.prompt([
+        {
+            name: "fname",
+            type: "input",
+            message: chalk.bold.yellow(`What is your name?`),
+            validate: (fname) => {
+                if (!isNaN(fname)) {
+                    return chalk.red.bold(`Please enter valid name`);
+                }
+                else {
+                    return true;
+                }
+            },
+        },
+    ]);
+    //for loop
+    for (let i = 0; i < data.length; i++) {
+        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+        let ans = await inquirer.prompt([
+            {
+                name: "quiz",
+                type: "list",
+                message: data[i].question,
+                choices: answers.map((val) => val),
+            },
+        ]);
+        if (ans.quiz == data[i].correct_answer) {
+            ++scoreNum;
+            console.log(`${chalk.bold.italic.blue("Correct")}`);
+        }
+        else {
+            console.log(`Correct Answer ${chalk.bold.italic.red(data[i].correct_answer)}`);
+        }
+    }
+    console.log(`Dear ${chalk.bold.italic.underline.blue(totlecaseName(name.fname))} \nYour Score ${chalk.green.bold(scoreNum)} out of ${chalk.red.bold(data.length)}`);
+};
+startQuiz();
+//! Quiz link
+//https://opentdb.com/api_config.php
